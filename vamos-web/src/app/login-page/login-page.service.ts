86c4655ea0a9d1821df62fa63d08170bc55094c2
jeonghan.ji <wlwjdgks123@gmail.com>
2019-12-15 19:34:29 +0900

    Update the get request to post request

--- vamos-web/src/app/login-page/login-page.service.ts
+++ vamos-web/src/app/login-page/login-page.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginPageService {
+  
+  constructor(private http: HttpClient) {
 
-  constructor(private http: HttpClient) { }
+  }
 
   getLogin(email: string, pwd: string) {
     console.log(`127.0.0.1:8000/login?email=${email}&pwd=${pwd}`);
-    return this.http.get<JSON>(`http://127.0.0.1:8000/login?email=${email}&pwd=${pwd}`);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'my-auth-token'
+      })
+    };
+    const body = {
+      "email": email,
+      "pwd": pwd
+    }
+    return this.http.post<JSON>(`http://127.0.0.1:8000/login`, body, httpOptions);
   }
 
 }
