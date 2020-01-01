f5faee2f63eed6550f97ac0fe142889c0de63fec
sonyuseon <altusdnjs@naver.com>
2020-01-02 01:16:20 +0900

    Update sequelize DB
    - Add auto Increment Id
    - Create groupInfo and voteDetail Table

---
+++ server/model/group.js
@@ -0,0 +1,63 @@
+module.exports = (sequelize, DataTypes) =>{
+    return meeting.define('group',{
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name:{
+            type : DataTypes.STRING(100),
+            allowNull : false
+        },
+        descript: {
+            type: DataTypes.STRING(1000),
+            allowNull: false
+        },
+        groupHostId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        groupDates : { // 모임 후보 날짜
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        groupMemberN :{
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        voteMemberN :{
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        voteCreatedDate:{
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        voteEndDate:{
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        voteFixedDate:{
+            type: DataTypes.STRING(20),
+            allowNull: true
+        },
+        voteURL:{
+            type: DataTypes.STRING(20),
+            allowNull: false
+        }
+        // TODO : 만료일 추가
+        // expiredTime:{
+        //     type: DataTypes.STRING(20),
+        //     allowNull: true
+        // }
+    }, {timestamps : false},
+    {
+        validate: {
+            voteEndDateAfterNow() {
+            if (this.voteEndDate.isAfter(this.voteCreatedDate)) {
+              throw new Error('voteEndDate date must be after now.');
+            }
+          }
+        }
+      });
+}
--- server/model/user.js
+++ server/model/user.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize, DataTypes) =>{
     return sequelize.define('user',{
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         email:{
             type : DataTypes.STRING(100),
             allowNull : false,
